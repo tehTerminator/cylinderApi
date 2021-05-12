5002ec211774c11cf54388ff7711c589dc11d10d
Prateek Kher <prateek.kher@gmail.com>
2021-05-12 12:55:30 +0530

    Project Initialized

---
+++ app/Models/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Lumen\Auth\Authorizable;
+
+class User extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable, HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'username',
+        'password',
+        'designation_id',
+        'department_id',
+        'is_administrator',
+        'mobile'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+    ];
+
+    public function department(){
+        return $this->hasOne(Department::class);
+    }
+
+    public function designation(){
+        return $this->hasOne(Designation::class);
+    }
+}
---
+++ database/migrations/2021_05_12_070716_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('username', 50)->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('designation_id')->nullable()->default(NULL);
+            $table->unsignedBigInteger('department_id')->nullable()->default(NULL);
+            $table->boolean('is_administrator')->default(FALSE);
+
+            $table->foreign('designation_id')->references('id')->on('designations');
+            $table->foreign('department_id')->references('id')->on('departments');
+            $table->string('mobile', 10)->nullable()->default(NULL);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
