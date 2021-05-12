3f8de203fc3ae9e3b1038a1924b1420f99a0ed3b
Prateek Kher <prateek.kher@gmail.com>
2021-05-12 13:54:49 +0530

    Created Migration

---
+++ database/migrations/2021_05_12_081634_create_beds_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beds', function (Blueprint $table) {
+            $table->id();
+            $table->integer('bed_number');
+            $table->unsignedBigInteger('ward_id');
+            $table->boolean('has_line')->default(false);
+            $table->foreign('ward_id')->references('id')->on('ward');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beds');
+    }
+}
